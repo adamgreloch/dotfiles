1e44f676b8b1eb3ecf64b90ebd745d21fc9dd299
Adam Greloch <zplhatesbananas@gmail.com>
2020-07-03 15:53:25 +0200

    dual monitor setup for polybar, compton/i3 config additions

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,10 +8,12 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 if type "xrandr" > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main -c ~/.config/polybar/config &
+    MONITOR=$m polybar --reload main-dp -c ~/.config/polybar/config &
+		MONITOR=$m polybar --reload main-hdmi -c ~/.config/polybar/config &
   done
 else
-  polybar --reload main -c ~/.config/polybar/config &
+  polybar --reload main-dp -c ~/.config/polybar/config &
+  polybar --reload main-hdmi -c ~/.config/polybar/config &
 fi
 
 echo "polybars launched..."
