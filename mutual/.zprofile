025d0a8c843082bc0dc9dbe2e31a4ed8a01035d7
Adam Greloch <zplhatesbananas@gmail.com>
2022-07-23 18:35:54 +0200

    xmonad dump

--- mutual/.zprofile
+++ mutual/.zprofile
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PATH="$PATH:/home/adam/.local/bin"
+export PATH="$PATH:/home/adam/scripts"
+
 [ -z "$DISPLAY" ] && [ $XDG_VTNR -eq 1 ] && exec xinit -- vt1 &> ~/.xsession-errors
 
 export GPG_TTY=$(tty)
--- mutual/xmonad/xmonad.hs
+++ mutual/xmonad/xmonad.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 
+{-
+ - TODO:
+ - [ ] Fix xmobar not appearing after initial exec. Though it will always
+ - appear after xmonad reload.
+ - [ ] One config for two screen sizes (desktop/laptop)
+ -}
+
 import XMonad
 
 import XMonad.Util.EZConfig
@@ -10,9 +17,14 @@ import System.Exit
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
 
+import XMonad.Layout.NoFrillsDecoration
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
+
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Magnifier
+import XMonad.Layout.Renamed
 
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
@@ -20,12 +32,57 @@ import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.Loggers
 
+
+-- WIP colors and some ideas from Ethan Schoonover
+-- https://github.com/altercation/dotfiles-tilingwm
+
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green   = "#859900"
+
+active      = blue
+activeWarn  = red
+inactive    = base02
+focusColor  = blue
+unfocusColor = base02
+
 main :: IO ()
 main = xmonad
      . ewmhFullscreen
      . ewmh
      . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
      $ myConfig
+
+topbar      = 5
+border      = 0
+gap         = 5
+bGap        = 90        -- bigGap, mainly for zen layout
+vbGap       = 240       -- vertical bigGap, mainly for zen layout
+
+topBarTheme = def
+    { inactiveBorderColor   = base03
+    , inactiveColor         = base03
+    , inactiveTextColor     = base03
+    , activeBorderColor     = active
+    , activeColor           = active
+    , activeTextColor       = active
+    , urgentBorderColor     = red
+    , urgentTextColor       = yellow
+    , decoHeight            = topbar
+    }
 
 myXmobarPP :: PP
 myXmobarPP = def
@@ -56,13 +113,21 @@ myXmobarPP = def
     red      = xmobarColor "#dc322f" ""
     lowWhite = xmobarColor "#586e75" ""
 
-myLayout = toggleLayouts Full (tiled ||| Mirror tiled ||| threeCol)
+myLayout = toggleLayouts myFull (myTiled ||| myMirror ||| threeCol ||| zen)
   where
-    threeCol = magnifiercz' 1.5 $ ThreeColMid nmaster delta ratio
+    myFull   = renamed [Replace "F"] $ Full
+    myTiled  = renamed [Replace "T"] $ addTopBar $ tiled
+    myMirror = renamed [Replace "MT"] $ addTopBar $ Mirror $ tiled
+    zen      = renamed [Replace "Z"] $ addBGaps $ Full
+    threeCol = renamed [Replace "TC"] $ addTopBar $ magnifiercz' 1.5 $ ThreeColMid nmaster delta ratio
     tiled    = Tall nmaster delta ratio
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
+    addTopBar            = noFrillsDeco shrinkText topBarTheme
+    addGaps              = gaps [(U, gap),(D, gap),(L, gap),(R, gap)]
+    addBGaps             = gaps [(U, bGap),(D, bGap),(L, vbGap),(R, vbGap)]
+    addSpacing           = spacing gap
 
 -- | The xmonad key bindings. Add, modify or remove key bindings here.
 --
@@ -72,7 +137,7 @@ myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- launching and killing programs
     [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
-    , ((modMask,               xK_p     ), spawn "dmenu_run") -- %! Launch dmenu
+    , ((modMask,               xK_p     ), spawn "dmenu-dark") -- %! Launch dmenu
     , ((modMask .|. shiftMask, xK_p     ), spawn "gmrun") -- %! Launch gmrun
     , ((modMask .|. shiftMask, xK_q     ), kill) -- %! Close the focused window
 
@@ -115,7 +180,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-shift-[1..9] %! Move client to workspace N
     [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]] -- changed from GreedyView
     ++
     -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
@@ -128,14 +193,29 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((shiftMask,      xF86XK_MonBrightnessDown), spawn "brightlight -p -w 1"  )
     , ((shiftMask,      xF86XK_MonBrightnessUp  ), spawn "brightlight -p -w 100")
     ]
+    ++
+    [ ((0,              xF86XK_AudioPlay     ), spawn "playerctl play-pause"  )
+    , ((0,              xF86XK_AudioPause    ), spawn "playerctl pause"  )
+    , ((0,              xF86XK_AudioNext     ), spawn "playerctl next"  )
+    , ((0,              xF86XK_AudioPrev     ), spawn "playerctl previous")
+    ]
+    ++
+    [ ((0,              xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
+    , ((0,              xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
+    , ((0,              xF86XK_AudioMute       ), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+    ]
+    ++
+    [ ((0,              xK_Print), spawn "maim -s -u | xclip -selection clipboard -t image/png")
+    ]
 
 myConfig = def
     { modMask        = mod4Mask  -- Rebind Mod to the Super key
     , terminal       = "urxvt"
     , keys           = myKeys
     , layoutHook     = myLayout
+    , borderWidth    = border
     }
     `additionalKeysP`
-    [ ("M-S-f", spawn "firefox" )
-    , ("M-S-m", spawn "thunderbird" )
+    [ ("M-C-f", spawn "firefox" )
+    , ("M-C-m", spawn "thunderbird" )
     ]
